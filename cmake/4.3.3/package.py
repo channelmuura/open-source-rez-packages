a3858143b388d24e12b05154f449196285efa4f7
Jonah Newton <jonah.newton@localhost.localdomain>
2026-06-14 16:37:31 +1000

    cmake tools

--- cmake/4.3.3/package.py
+++ cmake/4.3.3/package.py
@@ -4,6 +4,8 @@ name = 'cmake'
 
 version = '4.3.3'
 
+tools = ['cmake', 'cpack', 'ctest']
+
 build_requires = ['gcc', 'cmake']
 
 variants = [['platform-linux', 'arch-x86_64', 'os-rocky-9.8', 'python-3.11+']]
